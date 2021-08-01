fc954d82d678ab373ac864ffc93aecb247f225f1
Jorgevc87 <jorgevc87@Mac-mini-de-Jorge.local>
2021-07-31 23:20:50 -0500

    impl

--- src/00003_Types_Operations.kt
+++ src/00003_Types_Operations.kt
@@ -1,3 +1,23 @@
 public fun main() {
+    //Any unit, and nothing types
+    val anyNumbe: Any = 42
+    val anyString: Any = "42"
 
+    add()
+    addUnit()
+
+    val triple = Triple(3,2,4)
+    println("El first value of triple is ${triple.first}")
+    println("El second value of triple is ${triple.second}")
+    println("El third value of triple is ${triple.third}")
+}
+
+fun add() {
+    val result = 2 + 2
+    println(result)
+}
+
+fun addUnit(): Unit {
+    val result = 2 + 2
+    println(result)
 }
---
+++ src/00004_BasicControlFlow.kt
@@ -0,0 +1,16 @@
+public fun main() {
+    println("Basic Control Flow")
+    val yes: Boolean = true
+    val no: Boolean = false
+
+    //val yes = true
+    //val no = false
+
+    val doesOneEquelTwo = (1 == 2)
+    val doesOneNotEqualTwo = (1 != 2)
+
+    val alsoTrue = !(1 == 2)
+
+    val isOneGreaterThanTwo = (1 > 2)
+    val isOneLessThanTwo = (1 < 2)
+}
